f0e0303ad1ac1bac2962d6b8e5d7384832c178bc
Ryan Cox <ryancox@rbc.local>
2012-09-22 21:26:50 -0400

    Started on circular array Queue and realized that getSize() hasn't been working on circular or linked list arrays.  Hmmmm...

--- src/LListQueue/queue_test.cpp
+++ src/LListQueue/queue_test.cpp
@@ -10,12 +10,15 @@ int main() {
 Queue* q = new Queue();
 
   cout << "Size is " << q->size() << endl;
-  cout << "Is empty: " << q->isEmpty() << endl;
+
+  cout << "Adding 0-5..." << endl;
 
   for (int i=0; i<10; i++) {
     //cout << i << endl;
     q->enqueue(i);
   }
+
+  cout << "Size is " << q->size() << endl;
 
 	cout << "Size is " << q->size() << endl;
 	cout << "Is empty: " << q->isEmpty() << endl;
