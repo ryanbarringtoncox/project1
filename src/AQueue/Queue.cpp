36b7bbe4d374af4a37b3523232cec0861c6dd6b9
core.editor <ryanbarrington@gmail.com>
2012-12-25 15:07:33 -0500

    Array-based queue throws floating point exception

--- src/AQueue/Queue.cpp
+++ src/AQueue/Queue.cpp
@@ -6,6 +6,7 @@ Queue::Queue() {
 	capacity=5;
 	theQueue = new int[capacity];
 	front=0;
+	back=0;
 	theSize=0;	
 }
 
@@ -14,59 +15,43 @@ Queue::~Queue() {
 }
 
 int Queue::dequeue() {
-	assert(theSize>0);
-	int temp = theQueue[front];
-	front=(front+1)%capacity;
-	theSize--;	
-	//if theSize is less than half capacity, reduce Queue by half.  Don't go below size of 2!
-	if (theSize<capacity/2 && theSize > 2) {
-		int prevCapacity=capacity;
-		capacity=capacity/2;
-		int* tempArray = new int[capacity];
+	assert(theSize > 0);	
+	if (theSize < capacity/2) {
+		int* tempArray = new int[capacity / 2];
+		int* oldArray = theQueue;	
 		for (int i=0; i<theSize; ++i) {
-			int temp = theQueue[front];
-			tempArray[i] = temp;
-			front=(front+1)%prevCapacity;
+			tempArray[i] = theQueue[front%capacity];
+			front++;
 		}
-		delete[] theQueue;
 		theQueue = tempArray;
-		back=theSize;
-		front=0;
+		delete[] oldArray;
+		front = 0;
+		back = theSize;
+		capacity = capacity / 2;
 	}
+	int temp = theQueue[front];
+	front = (front + 1)%capacity;
+	theSize--;
 	return temp;
 }
 
 void Queue::enqueue(int value) {
-	if (theSize==0) {
-		theQueue[front]=value;
-		back=front;	
-		theSize++;
-	}
-	else {		
-		if (theSize < capacity) {
-			back=(back+1)%capacity;
-			theQueue[back]=value;			
-			theSize++;
+	if (theSize >= capacity) {
+		int* tempArray = new int[capacity * 2];
+		int* oldArray = theQueue;
+		for (int i=0; i<theSize; ++i) {
+				tempArray[i] = theQueue[front%capacity];
+				front++;
 		}		
-		else {
-			int prevCapacity=capacity;
-			capacity=capacity*2;
-			int* tempArray = new int[capacity];
-			for (int i=0; i<theSize; ++i) {
-				//int temp=this->dequeue();
-				int temp = theQueue[front];
-				tempArray[i] = temp;
-				front=(front+1)%prevCapacity;
-			}
-			//the order of these next two lines is CRUCIAL!  had em swapped before
-			delete[] theQueue;
-			theQueue=tempArray;
-			back=theSize;
-			theQueue[back]=value;
-			theSize++;
-			front=0;
-		}
-	}
+		theQueue = tempArray;
+		delete[] oldArray;
+		front = 0;
+		back = theSize;
+		capacity *= 2;
+	}  
+	theQueue[back] = value;
+	back = (back+1)%capacity;
+	theSize++;
 }
 
 int Queue::isEmpty() {
